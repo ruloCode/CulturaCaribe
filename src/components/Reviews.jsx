7a8de01335349f6a71751956d999fcc5b2752339
rulocode <andres@vitau.mx>
2023-02-14 15:54:54 -0500

    feat(reviews): adding reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ProfilePhoto from '../assets/profile_photo.jpg'
 // Icons
 import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 
@@ -6,18 +7,14 @@ const Reviews = () => {
   return (
     <div className="p-8 flex flex-col gap-8 bg-gray-100">
       <h1 className="text-[40px] text-center font-black">
-        Let’s hear What they says
+        Viviendo la cultura
       </h1>
       <div className="flex justify-center gap-4">
         <span className="text-5xl text-primary">
           <RiDoubleQuotesL />
         </span>
         <p className="max-w-2xl text-center text-gray-500">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus vel
-          lobortis tincidunt fames quisque mauris at diam. Nullam morbi ipsum
-          turpis amet id posuere torto quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore.
+        Cultura caribe es un portal en alianza entre tres organizaciones culturales que busca difundir y proyectar actividades, procesos y proyectos culturales con enfoque poblacional.
         </p>
         <span className="text-5xl text-primary">
           <RiDoubleQuotesR />
@@ -25,31 +22,19 @@ const Reviews = () => {
       </div>
       <div className="flex flex-col items-center justify-center gap-8">
         <div className="flex items-center justify-center gap-8 md:gap-12">
+      
+         
           <img
-            src="https://img.freepik.com/foto-gratis/empresaria-confiada-sonriente-que-presenta-brazos-cruzados_1262-20950.jpg"
-            className="w-8 h-8 md:w-14 md:h-14 object-cover rounded-full"
-          />
-          <img
-            src="https://img.freepik.com/foto-gratis/hombre-barbudo-feliz-sorprendido-camisa-apuntando-lejos_171337-5021.jpg"
-            className="w-10 h-10 md:w-16 md:h-16 object-cover rounded-full"
-          />
-          <img
-            src="https://img.freepik.com/foto-gratis/hombre-negocios-dueno-empresa-oficina_1303-15851.jpg"
+            src={ProfilePhoto}
             className="w-12 h-12 md:w-20 md:h-20 object-cover rounded-full ring-4 ring-primary p-1 bg-white"
           />
-          <img
-            src="https://img.freepik.com/foto-gratis/retrato-hombre-afroamericano-inteligente-profesional-pie-manos-cruzadas-sobre-pecho-pose-confianza_176420-33861.jpg"
-            className="w-10 h-10 md:w-16 md:h-16 object-cover rounded-full"
-          />
-          <img
-            src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg"
-            className="w-8 h-8 md:w-14 md:h-14 object-cover rounded-full"
-          />
+       
+        
         </div>
         <div>
-          <h3 className="text-center text-[24px] font-bold">Ricky Aprilia</h3>
+          <h3 className="text-center text-[24px] font-bold">Carlos Rodriguez Moreno</h3>
           <h5 className="text-center text-[20px] text-gray-500">
-            Founder of Varibo
+            Director
           </h5>
         </div>
       </div>
